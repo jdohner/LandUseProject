8a9aa2605c116aa401ca4dca3ce8333b93c451c9
Julia Dohner <juliadohner@jdohner-imac.ucsd.edu>
2018-07-09 15:59:46 -0700

    venturing into the weeds with messing with the fitting time frames

---
+++ getTimeFrame.m
@@ -0,0 +1,26 @@
+% getTimeFrameVec.m
+%
+% july 9, 2018
+% julia dohner
+%
+% create vectors for different fitting time frames
+
+function [timeFrameVec] = getTimeFrame(timeFrame,year);
+
+timeFrameVec = [year,ones(length(year),0)];
+
+if strcmp(timeFrame,'a')
+    i = find(year == 1900);
+    j = find(year == 2015.5);
+    timeFrameVec(i:j,2) = 1;
+    
+elseif strcmp(timeFrame,'b')
+    i = find(year == 1900);
+    j = find(year == 2005.5);
+    timeFrameVec(i:j,2) = 1;
+    
+elseif strcmp(timeFrame,'c')
+    i = find(year == 1950);
+    j = find(year == 1980);
+    timeFrameVec(i:j) = 1;
+end
