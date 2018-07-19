eeb67d9eefe5981bd743d8a6b2f7d9f5b47c625e
Julia Dohner <juliadohner@jdohner-imac.ucsd.edu>
2018-07-19 16:48:25 -0700

    one last change to gss5 before reworking the structure of the if statements in gss5. params still match those in ppt

--- getSourceSink5.m
+++ getSourceSink5.m
@@ -36,12 +36,13 @@ FF_2016mo(:,2) = FF_2016mo_0*d; %convert to pp
 if LU_i == 1 || LU_i == 4 || LU_i == 5
     
     data = csvread('HoughLU_perscomm_2016.csv'); 
-    LU_2016mo_0 = (interp1(data(:,1),data(:,2),year));
+    dataInterp = (interp1(data(:,1),data(:,2),year));
     LU_2016mo(:,1) = year;
-    LU_2016mo(:,2) = LU_2016mo_0*d*d1; %convert from TgC to PgC to ppm
+    LU_2016mo(:,2) = dataInterp*d*d1; %convert from TgC to PgC to ppm
     % same to here
     
     if LU_i == 4
+        clear LU_2016mo;
         % constant land use case
         % complicated because houghton above is interpolated to start and
         % end years dictated by driver code, but want a linear
@@ -54,9 +55,15 @@ if LU_i == 1 || LU_i == 4 || LU_i == 5
         i = find(LUconst_2016mo(:,1) == 1920);
         
         LUconst_2016mo(i:end,2) = LUconst_2016mo(i,2);
-        LU_2016mo = LUconst_2016mo;
+        
+        i2 = find(LUconst_2016mo == year(1));
+        j2 = find(LUconst_2016mo == year(end));
+        LU_2016mo = LUconst_2016mo(i2:j2,:);
+       
    
     elseif LU_i == 5
+        
+        clear LU_2016mo;
         % constant*2 land use case
         const2 = [data(1,1) data(1,2)*d*d1 ; data(end,1) 2*data(end,2)*d*d1];
         constYr2 = (const2(1,1):1/ts:const2(2,1))';
@@ -65,7 +72,10 @@ if LU_i == 1 || LU_i == 4 || LU_i == 5
         LUconst2_2016mo(:,2) = (interp1(const2(:,1),const2(:,2),constYr2));
         i = find(LUconst2_2016mo(:,1) == 1920);
         LUconst2_2016mo(i:end,2) = LUconst2_2016mo(i,2);
-        LU_2016mo = LUconst2_2016mo;
+        
+        i2 = find(LUconst2_2016mo == year(1));
+        j2 = find(LUconst2_2016mo == year(end));
+        LU_2016mo = LUconst2_2016mo(i2:j2,:);
         
     end
     
@@ -73,8 +83,8 @@ else
     if LU_i == 2 % hansis
         % 1849-2016 | GtC/yr | annual
         data = csvread('Pongratz2016_GCP_meanPasture_peat.csv'); %Hansis GCP
-        LU_2016mo_0 = (interp1(data(:,1),data(:,2),year));
-        LU_2016mo = [year, LU_2016mo_0*d];
+        dataInterp = (interp1(data(:,1),data(:,2),year));
+        LU_2016mo = [year, dataInterp*d];
       
     elseif LU_i == 3 % hough 03
         % 1850-2016 | GtC/yr | monthly
@@ -93,8 +103,8 @@ else
     elseif LU_i == 6 % gcp
         % 1850-2016 | GtC/yr | annual
         data = csvread('GCPv1.3_historicalLU2016.csv');
-        LU_2016mo_0 = (interp1(data(:,1),data(:,2),year));
-        LU_2016mo = [year, LU_2016mo_0*d];
+        dataInterp = (interp1(data(:,1),data(:,2),year));
+        LU_2016mo = [year, dataInterp*d];
         
     elseif LU_i == 7 % hough 03 extratropical ("LR low")
         % 1850-2016 | GtC/yr | monthly
@@ -110,8 +120,8 @@ else
         fclose(fid);
         data = datacell{1};        
         % interpolate to monthly and extend to 1850 using 1860 value
-        LU_2016mo_0 = (interp1([1850;data(:,1)],[data(1,2);data(:,2)],year));
-        LU_2016mo = [year, LU_2016mo_0*d];
+        dataInterp = (interp1([1850;data(:,1)],[data(1,2);data(:,2)],year));
+        LU_2016mo = [year, dataInterp*d];
 
     elseif LU_i == 9 % CABLE higher, grazing & harvest (C loss)
         % 1850-2016 | GtC/yr | annual
@@ -120,34 +130,34 @@ else
         fclose(fid);
         data = datacell{1};
         % interpolate to monthly and extend to 1850 using 1860 value
-        LU_2016mo_0 = (interp1([1850;data(:,1)],[data(1,3);data(:,3)],year));
-        LU_2016mo = [year, LU_2016mo_0*d];
+        dataInterp = (interp1([1850;data(:,1)],[data(1,3);data(:,3)],year));
+        LU_2016mo = [year, dataInterp*d];
         
     elseif LU_i == 10 % LPX-Bern HYDE
         % 1850-2016 | GtC/yr | annual
         data = csvread('LPX-BERN_HydeLU_1860-2016.csv',1);
-        LU_2016mo_0 = (interp1([1850;data(:,1)],[data(1,2);data(:,2)],year));
-        LU_2016mo = [year, LU_2016mo_0.*d];
+        dataInterp = (interp1([1850;data(:,1)],[data(1,2);data(:,2)],year));
+        LU_2016mo = [year, dataInterp.*d];
         
     elseif LU_i == 11 % LPX-Bern LUH
         % 1850-2016 | GtC/yr | annual
         data = csvread('LPX-BERN_LUH_1860-2016.csv',1);
-        LU_2016mo_0 = (interp1([1850;data(:,1)],[data(1,2);data(:,2)],year));
-        LU_2016mo = [year, LU_2016mo_0.*d];
+        dataInterp = (interp1([1850;data(:,1)],[data(1,2);data(:,2)],year));
+        LU_2016mo = [year, dataInterp.*d];
         
     elseif LU_i == 12 % Yue et al. (2018)
         % 1500-2005 | GtC/yr | annual
         data = csvread('Yue_1500-2005.csv',1);
-        LU_2016mo_0 = (interp1(data(:,1),data(:,5),year));
-        LU_2016mo = [year, LU_2016mo_0*d];
+        dataInterp = (interp1(data(:,1),data(:,5),year));
+        LU_2016mo = [year, dataInterp*d];
         % annual 1500 to 2005
         % need first and 5th columns
         
     elseif LU_i == 13 % Yue et al. (2018) without age dynamics
         % 1500-2005 | GtC/yr | annual
         data = csvread('Yue_1500-2005_noAgeDyn.csv',1);
-        LU_2016mo_0 = (interp1(data(:,1),data(:,5),year));
-        LU_2016mo = [year, LU_2016mo_0*d];
+        dataInterp = (interp1(data(:,1),data(:,5),year));
+        LU_2016mo = [year, dataInterp*d];
         % annual 1500 to 2005
         % need first and 5th columns
         
@@ -218,8 +228,10 @@ end
 FF_start = find(FF_2016mo(:,1) == year(1));
 FF_end = find(FF_2016mo(:,1) == year(end));
 ff = FF_2016mo(FF_start:FF_end,:);
-LU_start = find(LU_2016mo(:,1) == year(1));
-LU_end = find(LU_2016mo(:,1) == year(end));
-LU = LU_2016mo(LU_start:LU_end,:);
+% LU_start = find(LU_2016mo(:,1) == year(1));
+% LU_end = find(LU_2016mo(:,1) == year(end));
+% LU = LU_2016mo(LU_start:LU_end,:);
+
+LU = LU_2016mo;
 
 end
