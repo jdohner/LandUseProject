da2d67d9073ce3d8eca4e07cdc1f418f6ab2d495
Julia Dohner <juliadohner@jdohner-imac.ucsd.edu>
2019-07-11 15:20:51 -0700

    condensed code in getObservedCO2_2wNoise to simplify and to allow dpCO2a to be calculated from start to end year

--- forwardDriver3.m
+++ forwardDriver3.m
@@ -25,7 +25,7 @@ clear all; %close all
 % M = loop Houghton, BLUE cases
 % N = generate new ensemble member (randomly pick from CO2a,FF,ocean)
 
-vary = 'A';
+vary = 'N';
 
 nEnsemble = 10; % only used for vary = 'N' case
 
@@ -135,7 +135,6 @@ end
 [ff, LU] = getSourceSink6(LU_i,BLUE_i,vary); % for updated FF & LU
 
 save('ff','ff')
-
 
 % calculate ocean sink from ff
 if strcmp(vary,'N')
